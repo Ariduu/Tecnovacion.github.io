1a9b83a087322f74d8a36571b4cb63e7abc52707
Rodrigo Sihues <81935669+RSIHUESY@users.noreply.github.com>
2021-05-04 21:07:11 -0500

    boton popup

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onscroll = function(){
+  if(document.documentElement.scrollTop > 100){
+    document.querySelector('.botonContainer')
+    .classList.add('show')
+  }
+  else{
+    document.querySelector('.botonContainer')
+    .classList.add('show')
+  }
+}

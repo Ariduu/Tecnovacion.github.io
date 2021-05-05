c36f306ec4885d7c20abfa157824bc88b6dff168
Rodrigo Sihues <81935669+RSIHUESY@users.noreply.github.com>
2021-05-05 11:04:36 -0500

    nuevo boton

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 window.onscroll = function(){
-  if(document.documentElement.scrollTop > 100){
+  if(document.documentElement.scrollTop > 300){
     document.querySelector('.botonContainer')
     .classList.add('show')
   }
   else{
     document.querySelector('.botonContainer')
-    .classList.add('show')
+    .classList.remove('show')
   }
 }
+
+document.querySelector('.botonContainer')
+.addEventListener('click', () => {  
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
+})

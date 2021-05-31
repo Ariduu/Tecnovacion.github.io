0296a2e3140397833d1aa85a6a7746f5743acef8
josec551 <81968906+josec551@users.noreply.github.com>
2021-05-31 10:08:02 -0500

    pag. web v8.8.1

--- 1_Videojuegos/Jose/Publicacion_1/index.js
+++ 1_Videojuegos/Jose/Publicacion_1/index.js
@@ -1,18 +1,14 @@
-function mostrarMensaje(){
+/*function mostrarMensaje(){
     alert('/♪');
 }
 
 var nombre = "Jose";
 var edad = "20";
-var claro = "123123";
+var claro = "123123";*/
+var listaNumeros = [300,200,500];
+var $sumaNumeros
 
-var concatenacion = nombre + " " + edad
-document.write(concatenacion);  
 
-var  listaNumeros = [300 , 500 , 600 ]
-var $sumarNumeros = listaNumeros[0] + listaNumeros[1] + listaNumeros[2]
-
-document.getElementById("aea").innerHTML="El resultado de la suma es" + $sumarNumeros
 
 
 
